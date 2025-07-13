387fb12cbe8ed88ef744225c461a2dab628775be
Anıl Burak Gökce <anilburakgokce@gmail.com>
2025-07-13 21:46:15 +0200

    Added find_root.py

---
+++ code/find_root.py
@@ -0,0 +1,34 @@
+def modexp(base, exp, mod):
+    if exp < 0:
+        print("Exponent is less than 0!")
+        return -1
+    if mod < 0:
+        print("Mod is less than 0!")
+        return -1
+    if base < 0:
+        print("Base is less than 0!")
+        return -1
+    if base == 0:
+        return 0
+    if base > mod:
+        base = base % mod
+
+    res = 1
+    for i in range(exp):
+        res = (res * base) % mod
+    return res
+
+
+n = 3329
+deg = 32
+
+root = 0
+root_set = False
+for i in range(n):
+    res = modexp(i, deg//2, n)
+    if(res == (n - 1)):
+        root = i
+        root_set = True
+        break
+
+print("root = " + (str(i) if root_set else "is not found!"))
